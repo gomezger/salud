6818a7b132b0ea621a6c4807572f9a7cfc14b741
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2019-11-17 12:40:05 -0300

    Migration opiniones terminado. Listo para testing.

---
+++ api/database/migrations/2019_11_17_152634_opinion_migration.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OpinionMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('=opinion', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre',100);
+            $table->string('email',100);
+            $table->string('telefono',20);
+            $table->longText('descripcion');
+            $table->integer('aprobado')->default(0);
+            $table->foreing('id_profesional')
+                ->references('id')->on('profesional')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('=opinion');
+    }
+}
