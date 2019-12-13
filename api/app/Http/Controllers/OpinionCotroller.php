926ec78e25f485a50e814133a169b54f0e143ffc
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2019-12-12 21:20:09 -0300

    Create de Opiniones Listo. Sin Test.

---
+++ api/app/Http/Controllers/OpinionCotroller.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Profesional; 
+use App\TipoProfesional;
+use App\Opinion;
+use App\Helpers\JwtAuth;
+
+class OpinionCotroller extends Controller
+{
+
+    public function insertOpinion(Request $request){
+        $hash = $request->header('Authorization',null);
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+        
+        if($checkToken){
+            //Recoger POST
+            $json = $request->input('json',null);
+            $params = json_decode($json);
+            $params_array = json_decode($json,true);
+
+            //Verificacion de datos
+            $nombre=(!is_null($json) && isset($params->nombre)) ? $params->nombre : null;
+            $email=(!is_null($json) && isset($params->email)) ? $params->email : null;
+            $telefono=(!is_null($json) && isset($params->telefono)) ? $params->telefono : null;
+            $descripcion=(!is_null($json) && isset($params->descripcion)) ? $params->descripcion : null;
+            $aprobado=(!is_null($json) && isset($params->aprobado)) ? $params->aprobado : null;
+            $profesional=(!is_null($json) && isset($params->profesional)) ? $params->profesional : null;
+
+            //chequeamos que tengamos datos por POST
+            if(!is_null($params_array)){
+                $validar= \Validator::make($params_array,[
+                    'nombre' => 'required',
+                    'email'=> 'required|email',
+                    'telefono' => 'required',
+                    'descripcion'=>'required',
+                    'aprobado'=>'required',
+                    'profesional'=>'required'
+                 ]);
+
+                 //si hay errores envio el error
+                 if($validar->fails()){
+                    $data = array(
+                        'status' => 'error',
+                        'code' => 400,
+                        'errores' => $this->errores($validar->errors()),
+                        'messages' => 'Campos no validos',
+                     );
+      
+                     return response()->json($data,200);  
+                 }
+                 // verificar que existe el profesional
+                 $profesional = Profesional::find($profesional);
+                 if(is_null($tipo_profesional)){
+                    $data = array(
+                        'status' => 'error',
+                        'code' => 400,
+                        'errores' => ['El profesional no existe'],
+                        'messages' => 'No existe el profesional ingresado',
+                     );
+                     return response()->json($data,200);  
+                 }
+
+                 //crear modelo
+                 $opinion = new Opinion;
+                 $opinion->nombre=$nombre;
+                 $opinion->email=$email;
+                 $opinion->telefono=$telefono;
+                 $opinion->descripcio=$descripcion;
+                 $opinion->aprobado=$aprobado;
+                 $opinion->id_profesional=$profesional;
+
+                 $opinion->save();
+                 $opinion->load('profesional');
+                 $data = array(
+                    'status' => 'success',
+                    'opinion' => $opinion,
+                    'code' => 200,
+                    'message' => 'Se agregó la nueva opinion'
+                 );
+                 return response()->json($data,200);  
+
+            }else{
+                //No hay datos por POST
+                $data = array(
+                    'status' => 'error',
+                    'code' => 400,
+                    'messages' => 'No hay datos por POST',
+                 );
+                 return response()->json($data,200); 
+            }
+
+        }else{
+            $data = array(
+                'status' => 'error',
+                'code' => 400,
+                'messages' => 'Fallo autentificacion',
+             );
+             return response()->json($data,200);         
+        }
+    }
+}
