e1e570fa40811088d686dc2779ea928800af9c24
Xeron081495 <germang08@hotmail.com>
2020-01-14 10:52:26 -0300

    [fix] Menu en celular
    Ocultar menu al cambiar de vista

--- client/src/app/web/componentes/header/header.component.ts
+++ client/src/app/web/componentes/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,15 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ocultarMenu(){
+    $('.navbar-toggler').addClass('collapsed');
+    $('.navbar-toggler').removeClass('active');
+    $(".navbar-toggler").attr("aria-expanded","false");
+    $('.navbar-collapse').addClass('collapsing');
+    $('.navbar-collapse').removeClass('show');
+    $('.navbar-collapse').removeClass('collapsing');
   }
 
 }
